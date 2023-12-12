bfd9b5dcc1fb79ecd2b8b25f5445ea525ba64800
Belkharfa <belkharfa.saad@gmail.com>
2023-12-12 15:31:29 -0800

    task

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-
-let list = require('./100-data').list; let newList = [];
-
-newList = list.map(function (n, i) { return n * i; });
-
-console.log(list); console.log(newList);
+let list = require('./100-data').list;
+let newList = list.map((current, index) => {
+  return (current * index);
+});
+console.log(list);
+console.log(newList);
